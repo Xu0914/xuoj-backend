1f9c97658075d476db7ef74c7a89743dfd171fa3
Xsj <2662196566@qq.com>
2024-05-07 17:18:56 +0800

    第一、二期

---
+++ src/main/java/com/yupi/xuoj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.xuoj.mapper;
+
+import com.yupi.xuoj.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 徐思捷
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2024-05-06 18:40:38
+* @Entity com.yupi.xuoj.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
